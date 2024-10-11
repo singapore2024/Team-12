f23ce274bb0c9338254054cc70d7b9fdb5489060
kylie-kiaying <kylie.kiayingchua@gmail.com>
2024-10-11 12:44:36 +0800

    Added users in backend

---
+++ be/app/utils/user_database.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+
+client = MongoClient(MONGO_DB_URL)
+db = client["users"]
+
+def get_db():
+    return db
+
