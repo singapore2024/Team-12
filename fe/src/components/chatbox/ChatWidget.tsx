fd2187fc41fa452b17f7fa79a9e62fd6076739e3
tankh99 <khanghou99@gmail.com>
2024-10-11 16:50:19 +0800

    feat: add chatbox

---
+++ fe/src/components/chatbox/ChatWidget.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useState } from "react";
+import { useChatbot } from "@/api/hooks/useChatbot"
+import { z } from "zod";
+import { MessageSquare, X } from "lucide-react";
+import ChatBox from "./ChatBox";
+
+type P = {
+
+}
+
+export default function ChatWidget({}: P) {
+    const {messages, currMessage, sendMessage, isGenerating } = useChatbot();
+    
+    const [showChat, setShowChat] = useState(false);
+
+    return (
+        <div className="App">
+            
+            <button onClick={() => setShowChat(!showChat)} className="fixed bottom-4 right-4 p-2 bg-blue-500 text-white rounded-full">
+                {!showChat ? <MessageSquare /> : <X className='' />}
+            </button>
+            {showChat 
+                ? <ChatBox messages={messages} currMessage={currMessage} onSubmit={sendMessage} loading={isGenerating} />
+                : null
+            }
+        </div>
+    );
+}
