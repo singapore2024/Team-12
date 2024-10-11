da660bd138f1c30e6177c0f95de7b8acb9dd7c37
tankh99 <khanghou99@gmail.com>
2024-10-11 12:24:45 +0800

    refactor: standardise auth route naming

--- fe/src/api/hooks/useAuth.ts
+++ fe/src/api/hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query";
-import { getCurrentUser, login } from "../auth";
+import { getCurrentUser, login, signIn, signOut, signUp } from "../auth";
 import { useToast } from "@/hooks/use-toast";
 import { AxiosError } from "axios";
 import { useRouter } from "next/navigation";
@@ -22,62 +22,77 @@ export function useGetCurrentUser() {
     });
 }
 
-export function useLogin() {
+export function useSignin() {
     const queryClient = useQueryClient();
-    const { toast } = useToast();
-    const router = useRouter()
-    return useMutation({
-        mutationFn: login,
-        onSuccess: (res) => {
-            const accessToken = res.data.token;
-            queryClient.invalidateQueries({ queryKey: ["me"] });
-            localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
-            toast({
-                title: "Login successful",
-                variant: "success",
-            });
-            router.push("/admin")
-        },
-        onError: (err: AxiosError) => {
-            if (err.response!.status != 500) {
-                toast({
-                    title: "Error",
-                    description: "Invalid credentials",
-                    variant: "destructive",
-                });
-            } else {
-                toast({
-                    title: "Error",
-                    description: "An unknown error occurred",
-                    variant: "destructive",
-                });
-                console.error(err.response?.data);
-            }
-        }
-    });
-}
-
-export function useLogout() {
-    const {toast} = useToast();
     const router = useRouter();
+    const { toast } = useToast();
+  
     return useMutation({
-        mutationFn: async () => {
-            localStorage.removeItem(ACCESS_TOKEN_KEY);
-        },
-        onSuccess: () => {
-            toast({
-                title: "Logged out",
-                variant: "success",
-            });
-            router.push("/login");
-        },
-        onError: (err) => {
-            toast({
-                title: "Error",
-                description: "An unknown error occurred",
-                variant: "destructive",
-            });
-            console.error(err);
-        }
-    })
+      mutationFn: signIn,
+      onError: (err) => {
+        toast({
+          description: `${err.message}`,
+          title: "Error",
+          variant: "destructive",
+        });
+  
+        return true;
+      },
+      onSuccess: (accessToken) => {
+        toast({
+          title: "Login successful",
+          variant: "success",
+        });
+        localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+        queryClient.invalidateQueries({ queryKey: ["me"] });
+      },
+    });
+  }
+
+export function useSignup() {
+    const queryClient = useQueryClient();
+    const router = useRouter();
+    const { toast } = useToast();
+  
+    return useMutation({
+      mutationFn: signUp,
+      onError: (err) => {
+        toast({
+          description: `${err.message}`,
+          title: "Error",
+          variant: "destructive",
+        });
+      },
+      onSuccess: (user) => {
+        router.push("/account/login");
+        queryClient.invalidateQueries({ queryKey: ["me"] });
+        toast({
+          description:
+            "A new verification link has been sent to your email! " +
+            "Please check your Inbox/Spam folder for the link, " +
+            "and use it to verify your email. It will expire in 30 minutes.",
+          title: "Welcome " + user.email + "!",
+          variant: "success",
+        });
+      },
+    });
+  }
+
+export function useSignout() {
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: signOut,
+    onSuccess: async () => {
+      localStorage.removeItem(ACCESS_TOKEN_KEY);
+      await queryClient.invalidateQueries({ queryKey: ["me"] });
+    //   Cookies.remove(ACCESS_TOKEN_KEY);
+      toast({
+        title: "Logout successful",
+        variant: "success",
+      });
+    },
+  });
 }
