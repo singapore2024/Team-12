9b11b1b35420e82c2c6567d55255ac0d1ac60c65
xyT <tongyu5336@gmail.com>
2024-10-11 16:34:11 +0800

    Clean up

--- fe/src/app/marketplace/listing.tsx
+++ fe/src/app/marketplace/listing.tsx
@@ -1,37 +1,39 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { Listing } from '@/types/listing'
-import Image from 'next/image'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Listing } from "@/types/listing";
+import Image from "next/image";
 
 type P = {
-    listing: Listing
-}
+  listing: Listing;
+};
 
 export default function ListingCard({ listing }: P) {
-    const { image, name, price, quantity, description, authorName } = listing
-    return (
-        <Card>
-            <CardHeader>
-                <Image src={image} width={480} height={480} alt={name} />
-            </CardHeader>
-            <CardContent className='flex flex-col space-y-2'>
-                <div className='text-2xl'>
-                    {name}
-                </div>
-                <div className='font-bold'>${price}/{quantity}pc</div>
-                <div>
-                    {description}
-                </div>
-                <div className='flex items-center space-x-4'>
-                    <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar> <p>{authorName}</p>
-                </div>
-                <div className='italic'>
-                    Last posted: 2 days ago
-                </div>
-            </CardContent>
-        </Card>
-    )
+  const { image, name, price, quantity, description, authorName } = listing;
+  return (
+    <Card>
+      <CardHeader>
+        <Image src={image} width={480} height={480} alt={name} />
+      </CardHeader>
+      <CardContent className="flex flex-col space-y-2">
+        <div className="text-2xl">{name}</div>
+        <div className="font-bold">
+          ${price}/{quantity}pc
+        </div>
+        <div>{description}</div>
+        <div className="flex items-center space-x-4">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>{" "}
+          <p>{authorName}</p>
+        </div>
+        <div className="italic">Last posted: 2 days ago</div>
+        <div className="flex gap-10">
+          <Button className="bg-green-700">Purchase</Button>
+          <Button className="bg-green-700">Trade</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
