d90f51146e7c204a69a559dd9569f3e9f58b74b4
xyT <tongyu5336@gmail.com>
2024-10-11 16:19:24 +0800

    Fix background color

--- fe/src/app/marketplace/page.tsx
+++ fe/src/app/marketplace/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useGetListings } from "@/api/hooks/useListings";
 import Loading from "@/components/ui/loading";
@@ -11,27 +11,27 @@ import { Link } from "lucide-react";
 import { Button } from "react-day-picker";
 
 export default function MarketplacePage() {
-    // const {data: listings, isPending} = useGetListings()
-    const isPending = false;
+  // const {data: listings, isPending} = useGetListings()
+  const isPending = false;
 
   return (
-    <div className="p-12">
-        <h1 className="text-2xl leading-loose">Marketplace</h1>
-        <Separator className='my-4'/>
-        <div className='flex justify-between items-end space-x-4 mb-4'>
-            <div className='w-full'>
-                <Label className='mb-2 text-italic'>Search</Label>
-                <Input placeholder='Search for your listings' />
-            </div>
+    <div className="bg-white p-12">
+      <h1 className="text-2xl leading-loose text-black">Marketplace</h1>
+      <Separator className="my-4" />
+      <div className="flex justify-between items-end space-x-4 mb-4">
+        <div className="w-full text-black">
+          <Label className="mb-2 text-italic">Search</Label>
+          <Input placeholder="Search for your listings" />
         </div>
-        {isPending && <Loading/>}
-        {marketplaceListings && (
-            <div className="mx-auto">
-            {marketplaceListings.map((listing) => (
-                <ListingCard key={listing.id} listing={listing} />
-            ))}
-            </div>
-        )}    
+      </div>
+      {isPending && <Loading />}
+      {marketplaceListings && (
+        <div className="mx-auto">
+          {marketplaceListings.map((listing) => (
+            <ListingCard key={listing.id} listing={listing} />
+          ))}
+        </div>
+      )}
     </div>
-  )
+  );
 }
